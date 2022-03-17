72494922be477b4880228446b1c812842de453d3
vikram singh <vikram.singh@bold.com>
2022-03-17 17:13:28 +0530

    added sass support

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-console.log('hi');
+import './css/styles.scss'
+console.log('hiiiii');
 const x = { a: 1 };
 const y = { ...x, b: 2 };
 console.log(y);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const MCEP = require('mini-css-extract-plugin');
 const path = require('path');
 const mode = process.env.NODE_ENV == 'production' ? 'production' : 'development';
 module.exports = {
@@ -9,9 +10,22 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader'
         /* without additional settings, it refers to the .babelrc. It also looks at the .browserslistrc file to see how much backward compatibility to provide*/
+      },
+      {
+        test: /\.(s[ac]|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          // The order below is important. Works in the reverse order.
+          MCEP.loader,          //3. creates a separate css file instead of injecting into the DOM. Either use this or style-loader, but not both.
+          // 'style-loader',                    // 3.inject the css into the DOM (head probably)
+          'css-loader',                         // 2. converts the css into js
+          'postcss-loader',                      // looks at the .browserslistrc file
+          'sass-loader'                        // 1. compiles the scss into css
+        ]
       }
     ]
   },
+  plugins: [new MCEP()],
   devServer: {
     static: path.join(__dirname, 'dist')
   },
